52cf89c073aa9f4fc8897e820da524422ac2f294
EliteDevSolution <62186848+EliteDevSolution@users.noreply.github.com>
2021-12-01 00:30:15 +1000

    init

--- BinanceDetectMoonings.py
+++ BinanceDetectMoonings.py
@@ -447,8 +447,7 @@ def update_portfolio(orders, last_price, volume):
 
 def remove_from_portfolio(coins_sold):
     '''Remove coins sold due to SL or TP from portfolio'''
-    for coin in coins_sold:
-        coins_bought.pop(coin)
+    for coin in coins_sold:coins_bought.pop(coin)
 
     with open(coins_bought_file_path, 'w') as file:
         json.dump(coins_bought, file, indent=4)
